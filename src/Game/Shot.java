07c20282f07ad69eb589ec81d2a93ff290a353e2
Maik <Lokaler Benutzer@E-36-04.Schule.107842.mts-schule.de>
2012-11-12 12:38:49 +0100

    Movable Object hat eine neue Methode zum drehen eines Bildes

--- src/Game/Shot.java
+++ src/Game/Shot.java
@@ -1,5 +1,7 @@
 package Game;
-//bla
+/**
+ * @author Maik
+ */
 public class Shot {
-
+	
 }
--- src/MovableObject/MovableObject.java
+++ src/MovableObject/MovableObject.java
@@ -1,7 +1,24 @@
 package MovableObject;
+
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+import java.awt.image.BufferedImage;
+
 // SVN Test trial
 public abstract class MovableObject {
-public void paint(){
-	
-}
+	public void paint() {
+
+	}
+
+	private static BufferedImage rotateImage(BufferedImage src, double degrees) {
+		AffineTransform affineTransform = AffineTransform.getRotateInstance(
+				Math.toRadians(degrees), src.getWidth() / 2,
+				src.getHeight() / 2);
+		BufferedImage rotatedImage = new BufferedImage(src.getWidth(), src
+				.getHeight(), src.getType());
+		Graphics2D g = (Graphics2D) rotatedImage.getGraphics();
+		g.setTransform(affineTransform);
+		g.drawImage(src, 0, 0, null);
+		return rotatedImage;
+	}
 }
