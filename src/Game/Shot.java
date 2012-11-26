99efed3b1d225b04c7b6fbb5f7c7ee0cb5e3740f
Lokaler Benutzer <Lokaler Benutzer@E-36-05.Schule.107842.mts-schule.de>
2012-11-26 12:37:55 +0100

    Shot implementiert

--- src/Game/Shot.java
+++ src/Game/Shot.java
@@ -2,21 +2,25 @@ package Game;
 
 import java.awt.Graphics2D;
 
+import MovableObject.MovableObject;
+
 /**
  * @author Maik
  */
-public class Shot {
-	private int x;
-	private int y;
+public class Shot extends MovableObject {
+	private double x;
+	private double y;
+	private final static int length = 3;
 	private int degree;
+	private static int lifetime = 10;
 	
-	public Shot(int x, int y, int degree) {
+	public Shot(double x, double y, int degree) {
 		this.x = x;
 		this.y = y;
 		this.degree = degree;
 	}
 	
-	public int getX() {
+	public double getX() {
 		return x;
 	}
 
@@ -24,7 +28,7 @@ public class Shot {
 		this.x = x;
 	}
 
-	public int getY() {
+	public double getY() {
 		return y;
 	}
 
@@ -39,12 +43,21 @@ public class Shot {
 	public void setDegree(int degree) {
 		this.degree = degree;
 	}
+	
+	public int getLifetime() {
+		return lifetime;
+	}
+	
+	public void aktualisiereBewegung() {
+		x += length * Math.cos(Math.toRadians(degree));
+		y += length * Math.cos(Math.toRadians(degree));
+	}
 
 	public void paint() {
 		if (ViewBean.getSpielfeldGraphics() != null) {
 			Graphics2D spielfeldGraphic = ViewBean.getSpielfeldGraphics();
 			if (spielfeldGraphic != null) {
-				
+				spielfeldGraphic.fillOval((int)(x-0.5), (int)(y-0.5), length, length);
 				
 			}
 		}
